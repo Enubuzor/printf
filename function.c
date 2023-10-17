cd5e0367d5036efdd2009e7de460635c63c3a0e7
enubuzor <enubuzorchinedu@gmail.com>
2023-10-17 16:01:07 -0700

    printf

--- function.c
+++ function.c
@@ -33,46 +33,42 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int length = 0, i;
+	int length = 0;
 	char *str = va_arg(types, char *);
 
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	if (str == NULL)
-	{
-		str = "(null)";
-		if (precision >= 6)
-			str = "      ";
-	}
+	 UNUSED(flags);
+	 UNUSED(width);
+	 UNUSED(size);
+	 UNUSED(buffer);
 
-	while (str[length] != '\0')
-		length++;
-
-	if (precision >= 0 && precision < length)
-		length = precision;
-
-	if (width > length)
-	{
-		if (flags & F_MINUS)
-		{
-			write(1, &str[0], length);
-			for (i = width - length; i > 0; i--)
-				write(1, " ", 1);
-			return (width);
-		}
-		else
-		{
-			for (i = width - length; i > 0; i--)
-				write(1, " ", 1);
-			write(1, &str[0], length);
-			return (width);
-		}
-	}
-
-	return (write(1, str, length));
+	 if (str == NULL)
+	 {
+		 str = "(null)";
+		 if (precision >= 6)
+			 str = "      ";
+	 }
+	 while (str[length] != '\0')
+		 length++;
+	 
+	 if (precision >= 0 && precision < length)
+		 length = precision;
+	 
+	 if (width > length && !(flags & F_MINUS))
+	 {
+		 int i;
+		 for (i = width - length; i > 0; i--)
+			 write(1, " ", 1);
+	 }
+	 write(1, str, length);
+	 
+	 if (width > length && (flags & F_MINUS))
+	 {
+		 int i;
+		 for (i = width - length; i > 0; i--)
+			 write(1, " ", 1);
+	 }
+	 
+	 return ((width > length) ? width : length);
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
@@ -144,7 +140,7 @@ int print_int(va_list types, char buffer[],
 /************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
- * @types: Lista of arguments
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -155,34 +151,31 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
+	unsigned int n, i, count;
+    	unsigned int a[32];
 
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
+    	UNUSED(buffer);
+    	UNUSED(flags);
+    	UNUSED(width);
+    	UNUSED(precision);
+    	UNUSED(size);
 
-	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
+    	n = va_arg(types, unsigned int);
 
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
-}
+    	for (i = 0; i < 32; i++)
+    	{
+        	a[i] = (n >> (31 - i)) & 1;
+    	}
+
+    	for (i = 0, count = 0; i < 32; i++)
+    	{
+        	if (a[i] || i == 31)
+        	{
+            		char z = '0' + a[i];
+            		write(1, &z, 1);
+           		 count++;
+        	}
+    	}
+
+    return (count);
+}	
